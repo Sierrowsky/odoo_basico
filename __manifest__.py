6e5d82b5de8061507255ff4b82770822608cb538
Pablo Sierra <13sierras@gmail.com>
2023-11-21 14:26:50 +0100

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -24,8 +24,8 @@
 
     # always loaded
     'data': [
-        'views/suceso.xml'
         'views/informacion.xml',
+        'views/suceso.xml',
         'views/templates.xml',
         'views/menu.xml',
         'security/ir.model.access.csv',
